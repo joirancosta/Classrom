ea45c8136fdfcef4e465ea249948edd87a640769
“Joiran <joiran.costa.jc@gmail.com>
2025-06-18 18:09:21 -0300

    estudando armazenamento

--- aula04/api/src/middlewares/routeHandler.js
+++ aula04/api/src/middlewares/routeHandler.js
@@ -1,17 +1,21 @@
+import { Database } from "../database.js";
 import { routes } from "../routes.js";
 import { extractQueryParams } from "../utils/extract-query-params.js";
+
+const database = new Database()
 
 export function routeHandler(request, response){
     const route = routes.find((route) => {
         return route.method === request.method && route.path.test(request.url)
     })
+    
     if(route){
         const routeParms = request.url.match(route.path)
         const {query, ...params} = routeParms.groups
         request.params = params
         request.query = query ? extractQueryParams(query) : {}
-        return route.controller(request, response)
+        return route.controller({request, response, database})
     }
 
-    return response.writeHead(404).end("Rota não encontrada")
+    return response.writeHead(404).end("Nada funciona")
 }
