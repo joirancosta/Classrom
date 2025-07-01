3e4346ff0d54a5db4edc4fa0b76bb83ba8560c47
“Joiran <joiran.costa.jc@gmail.com>
2025-07-01 18:07:53 -0300

    Study auth

--- aula10/auth/src/controllers/products-controller.ts
+++ aula10/auth/src/controllers/products-controller.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from "express"
 
 class ProductsController {
-  async index(request: Request, response: Response) {}
+  async index(request: Request, response: Response) {
+    return response.json({message: "ok"})}
 
-  async create(request: Request, response: Response) {}
+  async create(request: Request, response: Response) {
+    return response.json({message: "ok"})}
 }
 
 export { ProductsController }
--- aula10/auth/src/controllers/sessions-controller.ts
+++ aula10/auth/src/controllers/sessions-controller.ts
@@ -1,7 +1,24 @@
+import { AppError } from "@/utils/AppError"
 import { Request, Response } from "express"
+import { authConfig } from "@/configs/auth";
+import { sign } from "jsonwebtoken";
+
 
 class SessionsController {
-  async create(request: Request, response: Response) {}
+  async create(request: Request, response: Response) {
+    const { username, password } = request.body
+    const fakeUser = {
+      id: 1,
+      username: "fulano",
+      password: "123456",
+    }
+    if (username !== fakeUser.username || password !== fakeUser.password) {
+      throw new AppError("Username e/ou senha incorreta", 401)
+    }
+    const { secret, expiresIn } = authConfig.jwt
+    const token = sign({}, secret, {expiresIn, subject: String(fakeUser.id)})
+    return response.json({token})
+  }
 }
 
 export { SessionsController }
